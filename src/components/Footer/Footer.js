c5dc08c4aa8260461d88e97cc32e9bf8d1a9e098
ahmetakyapi <ahmetakyapii@gmail.com>
2021-06-17 23:35:01 +0300

    Footer completed.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../Footer/Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+
+            <div className="footer-center">
+                <p>Tesla © 2021</p>
+                <p>Privacy & Legal</p>
+                <p>Contact</p>
+                <p>Careers</p>
+                <p>Get Newsletter</p>
+                <p>News</p>
+                <p>Forums</p>
+                <p>Locations</p>
+            </div>
+        </div>
+
+    )
+}
+export default Footer
